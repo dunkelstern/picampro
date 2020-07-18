7b12b5a9c1b23159f83f1b4dbfbbf55210b37b79
Johannes Schriewer <hallo@dunkelstern.de>
2020-07-18 03:23:25 +0200

    Refactor video pipeline to include tee object for streaming and recording at the same time

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -5,7 +5,9 @@ import sys
 
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst, GObject
+gi.require_version('GstBase', '1.0')
+
+from gi.repository import Gst, GObject, GstBase
 
 if TYPE_CHECKING:
     from settings import Settings
@@ -14,71 +16,104 @@ Gst.init(None)
 
 
 class Pipeline:
-    
-    pipeline = None
-    video_source = None
-    video_caps = None
-    video_caps_filter = None
-    video_queue = None
     audio_source = None       # TODO: audio source
     audio_caps = None         # TODO: audio caps
     audio_caps_filter = None  # TODO: audio caps filter
-    h264_parser = None
     muxer = None              # TODO: muxer
-    video_sink = None
     audio_sink = None         # TODO: audio sink
-    bus = None
 
     def __init__(self, settings: 'Settings'):
         self.settings = settings
         self.re_init()
 
     def re_init(self):
+        pipeline_items = []
+
         # Set up the gstreamer pipeline
         self.pipeline = Gst.Pipeline.new("pipeline")
 
         # video
         self.video_source = Gst.ElementFactory.make("rpicamsrc", "videosource")
+        pipeline_items.append(self.video_source)
 
         self.video_caps = Gst.Caps.from_string("video/x-h264, width=1920, height=1080, framerate=30/1,profile=high")
-        self.video_caps_filter = Gst.ElementFactory.make("capsfilter", "filter")
-        self.video_caps_filter.set_property("caps", self.video_caps)
-        self.video_queue = Gst.ElementFactory.make("queue", "videoqueue")
-        self.h264_parser = Gst.ElementFactory.make("h264parse", "videoparse")
-        self.video_sink = Gst.ElementFactory.make("fakesink", "videosink") # FIXME: only fakesink
+        pipeline_items.append(self.video_caps)
 
+        self.video_caps_filter = Gst.ElementFactory.make("capsfilter", "filter")
+        pipeline_items.append(self.video_caps_filter)
+
+        self.h264_parser = Gst.ElementFactory.make("h264parse", "videoparse")
+        pipeline_items.append(self.h264_parser)
+
+        self.video_tee  = Gst.ElementFactory.make('tee', 'video_tee')
+        pipeline_items.append(self.video_tee)
+
+        self.video_fake_queue = Gst.ElementFactory.make("queue", "videoqueue")
+        pipeline_items.append(self.video_fake_queue)
+
+        self.video_fake_sink = Gst.ElementFactory.make("fakesink", "videofakesink")
+        pipeline_items.append(self.video_fake_sink)
+
+        if None in pipeline_items:
+            print("ERROR: Not all gstreamer elements could be created")
+            sys.exit(1)
+
+        # TODO: add queue -> flvmux -> rtmpsink
+        # TODO: add queue -> matroskamux -> filesink
+
+        # Set video caps (frame size, etc.)
+        self.video_caps_filter.set_property("caps", self.video_caps)
+
+        # Video source properties
         self.video_source.set_property("preview", 1)
-        #video_source.set_property("rotation", 180)
         self.video_source.set_property("fullscreen", 0)
         self.video_source.set_property("preview-x", 0)
         self.video_source.set_property("preview-y", 0)
         self.video_source.set_property("preview-w", 904)  # FIXME: make dependend on screen size
         self.video_source.set_property("preview-h", 508)  # FIXME: make dependend on screen size
         self.video_source.set_property("inline-headers", 1)
-        #video_source.set_property("bitrate", 150000)
 
-        self.video_queue.set_property('max-size-bytes', 0)
-        self.video_queue.set_property('max-size-buffers', 0)
+        # Make sure the video pipeline always runs sync
+        self.video_fake_sink.set_property('sync', True)
 
         # TODO: audio
+        # Encoder: voaacenc
+        # Audio levels: level
 
-        if not self.pipeline or not self.video_source or not self.video_caps or not self.video_caps_filter or not self.h264_parser or not self.video_sink: 
-            print("ERROR: Not all gstreamer elements could be created") 
-            sys.exit(1)
-        
         # assemble pipeline
-        self.pipeline.add(self.video_source, self.video_caps_filter, self.video_queue, self.h264_parser, self.video_sink)
+        self.pipeline.add(
+            self.video_source,
+            self.video_caps_filter,
+            self.h264_parser,
+            self.video_tee,
+            self.video_fake_queue,
+            self.video_fake_sink
+        )
         self.video_source.link(self.video_caps_filter)
-        self.video_caps_filter.link(self.video_queue)
-        self.video_queue.link(self.h264_parser)
-        self.h264_parser.link(self.video_sink)
-        
+        self.video_caps_filter.link(self.h264_parser)
+        self.h264_parser.link(self.video_tee)
+
+        # Tee has dynamic pads, so request one for the fake sink
+        self.video_src_pad_template = Gst.PadTemplate.new_with_gtype(
+            "src_%u",
+            Gst.PadDirection.SRC,
+            Gst.PadPresence.REQUEST,
+            Gst.Caps.from_string('video/x-h264'),
+            GstBase.AggregatorPad.__gtype__
+        )
+        fake_video_src_pad = self.video_tee.request_pad(self.video_src_pad_template)
+        fake_queue_sink_pad = self.video_fake_sink.get_static_pad('sink')
+        fake_video_src_pad.link(fake_queue_sink_pad)
+        self.video_fake_sink.link(self.video_fake_queue)
+
+        # get the bus and connect signals
         self.bus = self.pipeline.get_bus()
         self.bus.add_signal_watch()
         self.bus.enable_sync_message_emission()
         self.bus.connect("message", self._on_message)
         self.bus.connect("sync-message::element", self._on_sync_message)
 
+        # now apply all settings to the stream
         self.settings.apply(self)
 
     def preview(self):
@@ -90,14 +125,14 @@ class Pipeline:
         if self.pipeline is None:
             self.re_init()
         self.pipeline.set_state(Gst.State.PLAYING)
-    
+
     def start_recording(self):
         """
         Start recording to disk, keeps the preview and streaming running
         """
         # TODO: start recording
         pass
-    
+
     def start_streaming(self):
         """
         Start streaming to a streaming server, keeps preview and recording running
